bb9b22cae8c5601daef61775e676d5422bad3565
Monicka Ruthrakumaran <monicka.rk@slenix.com>
2019-06-01 20:27:46 +0530

    Added dart report

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-var User = require('./user.js');
-
-
-
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -16,32 +12,43 @@ app.get('/', function (req, res) {
   res.send('Hello from server');
 })
 
-app.post('/enroll', function (req, res) {
-  console.log('Request body output '
-    + JSON.stringify(req.body));
+var mongoose = require('mongoose');
 
-  var mongoose = require('mongoose');
+// make a connection
+mongoose.connect('mongodb://localhost:27017/dart', { useNewUrlParser: true });
 
-  // make a connection
-  mongoose.connect('mongodb://localhost:27017/dart', { useNewUrlParser: true });
+// get reference to database
+var db = mongoose.connection;
 
-  // get reference to database
-  var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
-  db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Connection Successful!");
 
-  db.once('open', function () {
-    console.log("Connection Successful!");
-
-
+  app.post('/enroll', function (req, res) {
+    // console.log('Request body output '
+    //   + JSON.stringify(req.body));
     try {
-      db.collection("users").insertMany(req.body);
+      db.collection("tasks").insertMany(req.body);
       console.log("Data inserted successfully");
     } catch (e) {
       console.log(e);
     }
   });
 })
+
+app.get('/select', function (req, res) {
+  console.log("inside select");
+  db.collection("tasks").find({}).toArray(function (err, result) {
+    console.log("after select");
+    if (err) throw err;
+    res.status(200);
+    console.log(result);
+    res.json(result);
+  })
+})
+
+db.close();
 
 app.listen(PORT, function () {
   console.log('Server is running in ' +
